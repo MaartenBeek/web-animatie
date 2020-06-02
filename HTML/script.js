845ee0664d7e52624b6c9f11037a442b3752e3b2
Maarten <maartenbeek15@gmail.com>
2020-06-02 11:06:32 +0200

    update

--- HTML/script.js
+++ HTML/script.js
@@ -1,9 +1,39 @@
 document.addEventListener('keydown', function (event) {
     if (event.keyCode == 70) {
-        console.log("test")
         document.getElementById("water").style.height = "100vh";
+        document.getElementById("styleSVG").style.filter = "opacity(0.6)";
     }
     else if (event.keyCode == 82) {
         document.getElementById("water").style.height = "0";
+        document.getElementById("styleSVG").style.filter = "opacity(1)";
     }
 });
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode == 74) {
+        document.querySelector("#amerikaanse_vis .cls-6").style.cssText = "fill: white; transition: ease 1s;";
+        document.querySelector("#lichaam .cls-5").style.cssText = "fill: white; transition: ease 1s;";
+        document.querySelector("#oog circle").style.cssText = "fill: #d33541; transition: ease .5s;";
+        document.querySelector("#amerikaanse_vis #oog").style.cssText = "fill: #d33541; transition ease .5s;";
+    }
+    else if (event.keyCode == 85) {
+        document.querySelector("#amerikaanse_vis .cls-6").style.fill = "";
+        document.querySelector("#lichaam .cls-5").style.fill = "";
+        document.querySelector("#oog circle").style.fill = "";
+        document.querySelector("#amerikaanse_vis #oog").style.fill = "";
+    }
+});
+
+
+
+
+
+
+
+
+
+document.getElementById("japans_visje_14").addEventListener("click", swimAway);
+
+function swimAway() {
+    document.getElementById("japans_visje_14").classList.add("swimLeft");
+}
